d2f6055a6f12be8e41ab84ee7a9a3762fe239192
Italo1994 <italohortiz2015@gmail.com>
2022-03-12 02:27:21 -0300

    Adicionando seções de serviços e contato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
-const BASE_URL = 'https://api.pexels.com/v1/photos/'; // API PEXELS
+// const BASE_URL = 'https://api.pexels.com/v1/photos'; // API PEXELS
 
-const getPic = async () => {
-    const data = await fetch(BASE_URL)
-    .then( response => {
-        return response.json();
-    } )
-    .then( data => {
-        console.log(data);
-    })
-    .catch( (err) => console.log(err.message));
+// const getPic = async () => {
+//     const data = await fetch(BASE_URL, {
+//         method: 'GET',
+        
+//     })
+//     .then( response => {
+//         return response.json();
+//     } )
+//     .then( data => {
+//         console.log(data);
+//     })
+//     .catch( (err) => console.log(err.message));
 
-    return data.id;
-};
+//     return data.id;
+// }
 
-const loadImg = async () => {
-    const imgs = document.getElementsByClassName("img-api");
-    // for(let i=0; i<imgs.length; i++) {
-    //     imgs[i].src = await getPic();
-    //     // console.log(imgs[i].src);
-    // }
-    imgs[0].src = "https://api.pexels.com/v1/photos/" + getPic();
-    console.log(imgs[0].src);
+// const loadImg = async () => {
+//     const imgs = document.getElementsByClassName("img-api");
+//     // for(let i=0; i<imgs.length; i++) {
+//     //     imgs[i].src = await getPic();
+//     //     // console.log(imgs[i].src);
+//     // }
+//     // imgs[0].src = "https://api.pexels.com/v1/photos/" + getPic();
+//     // console.log(imgs[0].src);
+// }
+
+// loadImg();
+
+const content = document.querySelectorAll(".content");
+const qtdContent = content.length;
+
+console.log(content)
+for(let i=0; i<qtdContent; i++) {
+    if(i%2 !== 0) {
+        content[i].classList.toggle(".custom-border");
+    }
 }
-
-loadImg();
